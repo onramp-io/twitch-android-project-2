47470466ba7548d14d45b5bc7e2e9339c6647653
Joel Camargo Jr <jcamargo@pandora.com>
2022-04-23 16:00:21 -0400

    Remove comment

--- app/src/main/java/com/example/voyagerx/MainActivity.kt
+++ app/src/main/java/com/example/voyagerx/MainActivity.kt
@@ -8,6 +8,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // This is some change that i want to add to
     }
 }
