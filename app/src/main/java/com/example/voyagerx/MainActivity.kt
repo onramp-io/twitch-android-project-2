64179f180c41b323bbbd106f7e4daafdb976d4cf
Terry <103965030+brownyte@users.noreply.github.com>
2022-04-25 21:29:05 -0500

    Adding BottomNavigation functionality

--- app/src/main/java/com/example/voyagerx/MainActivity.kt
+++ app/src/main/java/com/example/voyagerx/MainActivity.kt
@@ -2,12 +2,41 @@ package com.example.voyagerx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.voyagerx.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavView : BottomNavigationView
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // This is some change that i want to add to
+        bottomNavView = binding.bottomNavigationView
+
+        val landingPageFragment = LandingPageFragment()
+        val profileFragment = ProfileFragment()
+        val settingsFragment = SettingsFragment()
+
+        changeFragmentView(landingPageFragment) //setting initial view to landing page as you only get here after bypassing login/register screens
+
+        bottomNavView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.landingPageFragment -> changeFragmentView(landingPageFragment)
+                R.id.profileFragment -> changeFragmentView(profileFragment)
+                R.id.settingsFragment -> changeFragmentView(settingsFragment)
+            }
+            true
+        }
     }
+
+    private fun changeFragmentView(desiredFragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame, desiredFragment)
+            commit()
+        }
 }
