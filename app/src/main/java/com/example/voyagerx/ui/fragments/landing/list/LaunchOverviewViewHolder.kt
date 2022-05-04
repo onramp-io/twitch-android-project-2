2a12045538dec05ed0a6065d62654180ec631a18
chrsbell <chrisbell.dev@gmail.com>
2022-05-03 18:13:53 -0700

    Update layout

--- app/src/main/java/com/example/voyagerx/ui/fragments/landing/list/LaunchOverviewViewHolder.kt
+++ app/src/main/java/com/example/voyagerx/ui/fragments/landing/list/LaunchOverviewViewHolder.kt
@@ -1,13 +1,14 @@
 package com.example.voyagerx.ui.fragments.landing.list
 
-import android.util.Log
-import android.view.View
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import coil.transform.RoundedCornersTransformation
+import coil.transform.Transformation
 import com.example.voyagerx.R
 import com.example.voyagerx.databinding.LandingPageOverviewCardBinding
 import com.example.voyagerx.repository.model.Launch
+import kotlin.math.round
 
 class LaunchOverviewViewHolder(private val binding: LandingPageOverviewCardBinding) :
     RecyclerView.ViewHolder(binding.root) {
@@ -16,11 +17,10 @@ class LaunchOverviewViewHolder(private val binding: LandingPageOverviewCardBindi
         binding.launchOverviewCardMissionName.text = item.mission_name
         binding.launchOverviewCardSiteName.text = item.launch_site_long
         binding.launchOverviewCardYear.text = item.launch_year
-        binding.launchImage.scaleType = ImageView.ScaleType.FIT_CENTER
-        binding.launchImage.load("https://live.staticflickr.com/65535/50630802488_8cc373728e_o.jpg") {
-            // placeholder image is the image used
-            // when our image url fails to load.
-            placeholder(R.drawable.ic_rocket_launch_24)
+        binding.launchImage.scaleType = ImageView.ScaleType.CENTER_CROP
+        binding.launchImage.load(item.image_links?.getOrNull(0)) {
+            transformations(RoundedCornersTransformation(10f,10f,10f,10f))
+            fallback(R.drawable.ic_rocket_launch_24)
         }
     }
 }
