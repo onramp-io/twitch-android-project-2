c7c284cae080409dc9f81167f0cfb05a881f518f
Terry <103965030+brownyte@users.noreply.github.com>
2022-05-04 12:02:38 -0500

    Adding unversioned files

---
+++ app/src/main/java/com/example/voyagerx/ui/fragments/userprofileac/FavoritesAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.voyagerx.ui.fragments.userprofileac
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.voyagerx.databinding.FavoritesOverviewCardBinding
+import com.example.voyagerx.repository.model.User
+import com.example.voyagerx.ui.fragments.userprofileac.FavoritesAdapter.*
+
+
+class FavoritesAdapter(
+    val user: User?) :
+    RecyclerView.Adapter<ViewHolder>() {
+
+
+    class ViewHolder(val binding: FavoritesOverviewCardBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun getItemCount(): Int {
+        return user?.favoriteLaunches!!.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = FavoritesOverviewCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        if (itemCount == 0) {
+            Log.d("user favorites", "= {$itemCount}")
+        } else {
+            holder.binding.launchOverviewCardMissionName.text = user!!.favoriteLaunches!![position].mission_name.toString()
+            holder.binding.launchOverviewCardSiteName.text = user.favoriteLaunches!![position].launch_site_long.toString()
+            holder.binding.launchOverviewCardYear.text = user.favoriteLaunches!![position].launch_year.toString()
+        }
+
+    }
+
+}
